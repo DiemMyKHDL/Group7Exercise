11d23d93fa8b137b6592bfdea5021b2373b4f311
DiemMyKHDL <lehoaitrinh67@gmail.com>
2023-11-11 21:34:47 +0700

    Exercise

---
+++ Exercise40.py
@@ -0,0 +1,9 @@
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+if (a==b) and (a==c) and (b==c):
+    print('The type of triangle: equilateral')
+elif (a==b) or (a==c) or (b==c):
+    print('The type of triangle: isoscelet')
+else:
+    print('The type of triangle: scalene')
